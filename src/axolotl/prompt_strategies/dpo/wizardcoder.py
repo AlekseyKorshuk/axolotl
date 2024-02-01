7ea22168852cda4c172f8df51ebbeaddc8fbcc6a
Aleksey Korshuk <ale-kor02@mail.ru>
2024-02-01 13:27:11 +0300

    updates

--- src/axolotl/prompt_strategies/dpo/wizardcoder.py
+++ src/axolotl/prompt_strategies/dpo/wizardcoder.py
@@ -25,7 +25,7 @@ def intel(cfg):  # pylint: disable=possibly-unused-variable,unused-argument
     def transform_fn(sample):
         sample["prompt"] = f"Below is an instruction that describes a task. " \
                            f"Write a response that appropriately completes the request.\n\n" \
-                           f"### Instruction:\n{sample['instruction']}\n\n### Response:\n"
+                           f"### Instruction:\n{sample['question']}\n\n### Response:\n"
         sample["chosen"] = f"{sample['chosen']}<|endoftext|>"
         sample["rejected"] = f"{sample['rejected']}<|endoftext|>"
         return sample
